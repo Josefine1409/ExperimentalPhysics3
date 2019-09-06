f08d86ed29c614f9f4aca37ccb7b1a7749fa8fbd
LukasBærbar <lukas.m.wick@gmail.com>
2019-09-06 16:03:17 +0200

    Forhåbenligt færdig atenuation

--- Comptonspredning/databehandling/Autenuation.m
+++ Comptonspredning/databehandling/Autenuation.m
@@ -9,6 +9,7 @@ for i=1:length(deltaThiknessLead)
     plateThiknessLead(i+1) = sum(deltaThiknessLead(end-i+1:end));
 end
 plateThikness = {[0,10.11,20.22,30.34,40.46,50.55,60.65,70.79,80.9],[plateThiknessLead],[0,1.02,2.06,3.05,4.04,5.06,6.10,7.09,8.12,9.11],[0,7.98,16.03,24.02]};
+plateThiknessUncertanty = 0.02;
 
 for i = 1:length(name)
     counts=zeros(size(plateThikness{i}));
@@ -17,37 +18,51 @@ for i = 1:length(name)
             path  = [pathStart num2str(j-1) 'Plates_ch001.txt'];
             [X,Y,Yerr] = hisFraData(path);
             counts(j) = sum(Y((channelInterval(1,1):channelInterval(1,2))));
-            figure
-            title([name{i},': measurement num: ',num2str(j),', T =',num2str(plateThikness{i}(j))])
-            hold on
-            plot(X,Y,'.')
-            xlabel('Channel')
-            ylabel('Counts')
-            plot(X([channelInterval(1,1):channelInterval(1,2)]),Y([channelInterval(1,1):channelInterval(1,2)]),'*')
+%             figure
+%             title([name{i},': measurement num: ',num2str(j),', T =',num2str(plateThikness{i}(j))])
+%             hold on
+%             plot(X,Y,'.')
+%             xlabel('Channel')
+%             ylabel('Counts')
+%             plot(X([channelInterval(1,1):channelInterval(1,2)]),Y([channelInterval(1,1):channelInterval(1,2)]),'*')
         end 
     figure
     hold on
-    xlabel('Thiknes of material [cm^-1]')
+    xlabel('Thikness of material [cm^-1]')
     ylabel('Counts under peak')
 
     cErr = sqrt(counts);
-    x = plateThikness{i};
-    y = counts;
+    x = plateThikness{i}
+    y = counts
     yerr = cErr;
-    errorbar(x,y,yerr,'.')
     beta0 = [y(1)-y(end),-0.1,y(end)];
-    linFun =@(beta,x) beta(1).*exp(beta(2).*x)+beta(3);
-%     plot(x,linFun(beta0,x))
+    linFun =@(beta,x) beta(1).*exp(x./beta(2))+beta(3);
+    plot(x,linFun(beta0,x))
     w = 1./yerr.^2;
-    [beta,R,J,CovB,MSE,ErrorModelInfo] = nlinfit(x,y,@(beta,x) linFun(beta,x),beta0,'weights',w);
+    [beta,R,J,CovB,MSE,ErrorModelInfo]  = nlinfit(x,y,@(beta,x) linFun(beta,x),beta0,'weights',w);
+    yerr = sqrt(yerr.^2 + (beta(1).*beta(2).*exp(beta(2).*x)*plateThiknessUncertanty).^2);
+    w = 1./yerr.^2;
+    errorbar(x,y,yerr,'.')
+%     [beta,R,J,CovB,MSE,ErrorModelInfo] = nlinfit(x,y,@(beta,x) linFun(beta,x),beta,'weights',w);
+    
     hold on
     xs = linspace(x(1),x(end)+2,1000);
     plot(xs,linFun(beta,xs))
-    us{i} = CovB/MSE;
-    mse{i} = MSE
-    pValue{i} = 1-chi2cdf(MSE*(length(y)-2),(length(y)-2))
-    title(['Fit of autenuation for material ',name{i},', P-value =',num2str(pValue{i})])
+    us = CovB/MSE;
+    US{i} = us;
+    mse{i} = MSE;
+    BETA{i} = beta
+    pValue= 1-chi2cdf(MSE*(length(y)-2),(length(y)-2));
+    PvALUE{i} = pValue;
+    title(['Fit of autenuation for material ',name{i},', P-value =',num2str(pValue)])
+    
+    disp(['Fit of atenuation for ',name{i}, ' gives tau = ', num2str(beta(2)),'\pm',num2str(us(2,2)),' cm^-1'])
+    disp(['and bacground = ', num2str(beta(3)),'+-',num2str(us(3,3)),' counts.'])
+    disp(['With MSE = ', num2str(MSE),' with p-value: ',num2str(pValue)])
+
 end
+
+
 
 function [X,Y,Yerr] = hisFraData(filename)
 delimiter = ' ';
