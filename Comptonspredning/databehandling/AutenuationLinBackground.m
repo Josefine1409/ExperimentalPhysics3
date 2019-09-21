469e624a25fcdcdd41a97288f56d447187458d81
Lukas Wick <lukas.m.wick@gmail.com>
2019-09-21 21:51:23 +0200

    Tilføjet nye filer, tilpasset kalibrering og merget
    Auto stash before merge of "master" and "origin/master"

--- Comptonspredning/databehandling/AutenuationLinBackground.m
+++ Comptonspredning/databehandling/AutenuationLinBackground.m
@@ -3,6 +3,8 @@ folderName = {'Aluminum','Lead','Cupper','Brass'};
 name = {'Al','Pb','Cu','Brass'};
 rho = [2.699e+00,1.135E+01,8.960e+00]
 mu_rho = [7.5055e-02,11.3663e-2,7.3103E-02]
+% mu_rho = [7.4642e-02,11.3663e-2,7.3103E-02]
+
 mu = mu_rho.*rho
 
 channelInterval= [[450,630],[450,630],[450,630],[450,630]];
@@ -12,7 +14,7 @@ for i=1:length(deltaThiknessLead)
     plateThiknessLead(i+1) = sum(deltaThiknessLead(end-i+1:end));
 end
 plateThikness = {[0,10.11,20.22,30.34,40.46,50.55,60.65,70.79,80.9]./10,[plateThiknessLead]./10,[0,1.02,2.06,3.05,4.04,5.06,6.10,7.09,8.12,9.11]./10,[0,14.19,29.87,44.42]./10,[0,7.98,16.03,24.02]./10};
-plateThiknessUncertanty = 0.01;
+plateThiknessUncertanty = 0.02;
 
 for i = 1:length(name)
     counts=zeros(size(plateThikness{i}));
@@ -31,13 +33,16 @@ for i = 1:length(name)
 %             ylabel('Counts')
 %             plot(X([channelInterval(1,1):channelInterval(1,2)]),Y([channelInterval(1,1):channelInterval(1,2)]),'*')
         end 
-    figure
-    hej = log(counts./counts(1));
-    plot(plateThikness{i},hej,'.')
-    figure
+%     figure
+%     hej = log(counts./counts(1));
+%     plot(plateThikness{i},hej,'.')
+    fig = figure
     hold on
-    xlabel('Thikness of material [cm]')
-    ylabel('Counts under peak')
+    xlabel('Thickness x [cm]')
+    ylabel('Counts under peak N')
+    set(gca, 'YScale', 'log')
+    set(gca,'FontSize',15) 
+
     x = plateThikness{i};
     y = counts;
     yerr = countUs;
@@ -57,22 +62,24 @@ for i = 1:length(name)
 %         CovB(2,2)
 %         MSE
 
-    errorbar(x,y,yerr,'.')
     hold on
-    xs = linspace(x(1),x(end)+2,1000);
-    plot(xs,linFun(beta,xs))
+    xs = linspace(x(1),x(end),1000);
+    plot(xs,linFun(beta,xs),'Linewidth',1.5)
+    errorbar(x,y,yerr,'.','markersize',10)
+    ylim([min(y)+(min(y)-max(y))/0.2-max(yerr),max(y)-(min(y)-max(y))/5])
     us = CovB/MSE;
     US{i} = us;
     mse{i} = MSE;
     BETA{i} = beta;
     pValue= 1-chi2cdf(MSE*(length(y)-2),(length(y)-2));
     PvALUE{i} = pValue;
-    title(['Fit of autenuation for material ',name{i},', P-value =',num2str(pValue)])
+%     title(['Fit of autenuation for ',name{i}])
     
     disp('_____________________________________________________________________________')
     disp(['Fit of atenuation for ',name{i}, ' gives mu = ', num2str(beta(2)),'+-',num2str(us(2,2)),' cm^-1'])
 %     disp(['and bacground = ', num2str(beta(3)),'+-',num2str(us(3,3)),' counts.'])
     disp(['With MSE = ', num2str(MSE),' with p-value: ',num2str(pValue)])
+    saveas(fig,['..\figure\Atenuation',name{i},'.png'])
 
 end
 
@@ -98,10 +105,10 @@ end
 
 function [counts,countUns]=gaussCounter(X,Y,Yerr,xmin,xmax,titleName)
 figure
-errorbar(X,Y,Yerr,'.')
 hold on
-xlabel('Channel')
-ylabel('Counts')
+xlabel('Channel Ch ')
+ylabel('Counts n')
+set(gca,'FontSize',15) 
 
     
 x = X;
@@ -118,19 +125,26 @@ y = y(lowerIndex);
 yerr = yerr(lowerIndex);
 
 beta0 = [(xmin+xmax)/2,(xmax-xmin)/3,max(y),0,20];
-plot(x,fitfunction(beta0,x))
+% plot(x,fitfunction(beta0,x))
 w = 1./yerr.^2;
 [beta,R,J,CovB,MSE,ErrorModelInfo] = nlinfit(x,y,@fitfunction,beta0,'weights',w);
-plot(x,fitfunction(beta,x));
-plot(x,beta(4).*x+beta(5));
+errorbar(X,Y,Yerr,'.','markersize',10)
+plot(x,fitfunction(beta,x),'Linewidth',1.5);
+plot(x,beta(4).*x+beta(5),'r--','Linewidth',1.5);
+legend('Data','Gauss fit','Background fit')
+
 us = CovB/MSE;
+us;
 pValue = 1-chi2cdf(MSE*(length(y)-5),(length(y)-5));
 
 
 % background = (xmax-xmin)*beta(5) + 1/2.*beta(4).*(xmax^2-xmin^2)
-title([titleName 'med p-value: ' num2str(pValue)])
+% title([titleName 'med p-value: ' num2str(pValue)])
 counts = abs(2*sqrt(pi)*beta(2)*beta(3));
-countUns = sqrt(counts+(2*sqrt(pi)*beta(3)).^2*us(2,2).^2+(2*sqrt(pi)*beta(2)).^2*us(3,3).^2);
+countUns = sqrt(counts+...
+    (2*sqrt(pi)*beta(3)).^2*us(2,2).^2+...
+    (2*sqrt(pi)*beta(2)).^2*us(3,3).^2+...
+    abs(2.*(2*sqrt(pi)*beta(2)).*(2*sqrt(pi)*beta(3))*us(2,3)));
 end
 function y = fitfunction(beta,x)
     y = beta(4).*x+beta(5)+abs(beta(3)).*exp(-((x-beta(1))./(2.*abs(beta(2)))).^2);
--- Comptonspredning/databehandling/GausFitterMedGrenser.m
+++ Comptonspredning/databehandling/GausFitterMedGrenser.m
@@ -2,17 +2,79 @@ clear all; close all;clc;
 filenames = {'Kalibrering_Co60_ch001.txt','Kalibrering_Cs137_ch001.txt','Kalibrering_Ra226_ch001.txt'};
 names = {'Co60','Cs137','Ra226'};
 
-peakValues = {[1.1732,1.3325],[0.6617],[0.35193,0.60931,0.76836,1.1203,1.7645]}
-peakBorders = {[905,1019;1019,1151],[500,600],[259,326;436,533;551,657;809,950;1293,1461]}
+peakValues = {[1.1732,1.3325],[0.6617],[0.35193,0.60931,0.76836,1.1203,1.7645]};
+peakBorders = {[905,1019;1019,1151],[500,600],[259,326;436,533;551,657;809,950;1293,1461]};
 
 
+%%
 filenames = {'Kalibrering_Ra226_ch001.txt'};
 names = {'Ra226'};
 
-peakValues = {[0.35193,0.60931,0.76836,1.1203,1.3777 ,1.7645]}
-peakBorders = {[259,326;436,533;551,657;809,950;1040,1120;1293,1461]}
+peakValues = {[0.35193,0.60931,0.76836,1.1203,1.3777 ,1.7645]};
+peakBorders = {[259,326;436,533;551,657;809,950;1040,1120;1293,1461]};
+
+[X,Y,Yerr] = hisFraData(filenames{1});
+data = fitGaussInSpectrum(X,Y,Yerr,names{1},peakValues{1},peakBorders{1});
+figure(3)
+hold on
+xlabel('Energy [MeV]')
+ylabel('Channel number')
+set(gca,'FontSize',15) 
+
+x =data(3,:);
+y = data(1,:);
+yerr = data(2,:);
+
+beta0 = [1,2];
+linFun =@(beta,x) (x-beta(2))/beta(1);
+w = 1./yerr.^2;
+[beta,R,J,CovB,MSE,ErrorModelInfo] = nlinfit(x,y,@(beta,x) linFun(beta,x),beta0,'weights',w);
+hold on
+plot(x,linFun(beta,x),'b','linewidth',1)
+errorbar(x,y,yerr,'.r','markersize',8)
+
+us = CovB/MSE
+MSE
+pValue = 1-chi2cdf(MSE*(length(y)-2),(length(y)-2))
+
+disp(['Energi som funktion af channel: E= ' num2str(beta(1)) '+-' num2str(us(1,1)) 'Channel-' num2str(beta(2)) '+-' num2str(us(2,2)) 'MeV'])
+filenames = {'Kalibrering_Co60_ch001.txt','Kalibrering_Cs137_ch001.txt'};
+names = {'Co60','Cs137'};
+
+peakValues = {[1.1732,1.3325],[0.66164]};
+peakBorders = {[905,1019;1019,1151],[500,600]};
+
+Data = [];
+
+for i = 1:length(filenames)
+    [X,Y,Yerr] =hisFraData(filenames{i});
+    Data = [Data, fitGaussInSpectrum(X,Y,Yerr,names{i},peakValues{i},peakBorders{i})];
+end
+figure(3)
+x =Data(3,:);
+y = Data(1,:);
+yerr = Data(2,:);
+
+w = 1./yerr.^2;
+[beta,R,J,CovB,MSE,ErrorModelInfo] = nlinfit(x,y,@(beta,x) linFun(beta,x),beta,'weights',w);
+hold on
+plot(data(3,:),linFun(beta,data(3,:)),'r','linewidth',1)
+errorbar(x,y,yerr,'.b','markersize',8);
+
+us = CovB/MSE
+mse = MSE
+pValue = 1-chi2cdf(MSE*(length(y)-2),(length(y)-2))
+
+disp(['Energi som funktion af channel: E= ' num2str(beta(1)) '+-' num2str(us(1,1)) 'Channel-' num2str(beta(2)) '+-' num2str(us(2,2)) 'MeV'])
+xlim([data(3,1)-0.05,data(3,end)+0.05])
 
 
+%%
+
+    
+filenames = {'Kalibrering_Ra226_300s_ch001.txt'};
+peakValues = {[0.1861,0.24198,0.29521,0.35192,0.60931]};
+peakBorders = {[137,168;183,222;222,263;263,316;440,540]};
 
 data = [];
 
@@ -20,26 +82,35 @@ for i = 1:length(filenames)
     [X,Y,Yerr] =hisFraData(filenames{i});
     data = [data, fitGaussInSpectrum(X,Y,Yerr,names{i},peakValues{i},peakBorders{i})];
 end
-figure
+figure(3)
 hold on
-xlabel('Energy')
-ylabel('Channel')
-x =data(3,:)
-y = data(1,:)
-yerr = data(2,:)
+xlabel('Energy [MeV]')
+ylabel('Channel number')
+set(gca,'FontSize',15) 
 
-errorbar(x,y,yerr,'.')
+x =data(3,:);
+y = data(1,:);
+yerr = data(2,:);
+
 beta0 = [1,2];
-linFun =@(beta,x) beta(1).*x+beta(2)
+linFun =@(beta,x) (x-beta(2))/beta(1);
 w = 1./yerr.^2;
-[beta,R,J,CovB,MSE,ErrorModelInfo] = nlinfit(x,y,@(beta,x) linFun(beta,x),beta0,'weights',w)
+[beta,R,J,CovB,MSE,ErrorModelInfo] = nlinfit(x,y,@(beta,x) linFun(beta,x),beta0,'weights',w);
 hold on
-plot(x,linFun(beta,x))
+plot(x,linFun(beta,x),'c','linewidth',1)
+errorbar(x,y,yerr,'.m','markersize',8)
+
 us = CovB/MSE
 MSE
 pValue = 1-chi2cdf(MSE*(length(y)-2),(length(y)-2))
 
-disp(['Energi som funktion af channel: E= ' num2str(beta(1)^-1) 'Channel-' num2str(beta(2)/beta(1)) 'MeV'])
+disp(['Energi som funktion af channel: E= ' num2str(beta(1)) '+-' num2str(us(1,1)) 'Channel-' num2str(beta(2)) '+-' num2str(us(2,2)) 'MeV'])
+
+legend('Data 2. day','Fit 2. day','Data 1. day','Fit 1. day','Data 3. day','Fit 3. day','Location','northwest')
+
+
+%%
+
 
 function [X,Y,Yerr] = hisFraData(filename)
 addpath('..\data\Kalibrering')
@@ -71,17 +142,23 @@ n = length(peakValue);
 
 figure
 errorbar(X*0.0012988-0.020395,Y,Yerr,'.')
-xlabel('MeV Ca.')
+% errorbar(X,Y,Yerr,'.','markersize',10)
+
+xlabel('Energy [MeV]')
+% xlabel('Channel number')
 ylabel('Counts')
+set(gca,'FontSize',15) 
+
 hold on
 for i = 1:n
     plot([peakValue(i),peakValue(i)],[0,500])
 end
 figure
-errorbar(X,Y,Yerr,'.')
+errorbar(X,Y,Yerr,'.','markersize',10)
 hold on
-xlabel('Channel')
+xlabel('Channel number')
 ylabel('Counts')
+set(gca,'FontSize',15) 
 title(name)
 
 for i = 1:n
@@ -106,26 +183,26 @@ beta0 = [(x2+x3)/2,(x3-x2)/3,max(y),0,20];
 % plot(x,fitfunction(beta0,x))
 w = 1./yerr.^2;
 [beta,R,J,CovB,MSE,ErrorModelInfo] = nlinfit(x,y,@fitfunction,beta0,'weights',w);
-beta(1)
-beta(2)
-plot(x,fitfunction(beta,x))
-plot(x,beta(4).*x+beta(5))
+beta(1);
+beta(2);
+plot(x,fitfunction(beta,x),'linewidth',2)
+% plot(x,beta(4).*x+beta(5))
 us = CovB/MSE;
-MSE
+mse =MSE;
 MSECount(i) = MSE;
 pValue(i) = 1-chi2cdf(MSE*(length(y)-5),(length(y)-5));
+P_Value = 1-chi2cdf(MSE*(length(y)-5),(length(y)-5));
 
-
-plot([beta(1)+us(1,1),beta(1)+us(1,1)],[0,max(y)])
-plot([beta(1)-us(1,1),beta(1)-us(1,1)],[0,max(y)])
+% plot([beta(1)+us(1,1),beta(1)+us(1,1)],[0,max(y)])
+% plot([beta(1)-us(1,1),beta(1)-us(1,1)],[0,max(y)])
 
 peakChannel(i) = beta(1,1);
 peakUns(i) = us(1,1);
 end
 
-peakUns
-peakChannel
-peakValue
+peakUns;
+peakChannel;
+peakValue;
 data = [peakChannel;peakUns;peakValue;pValue;MSECount];
 end
 
