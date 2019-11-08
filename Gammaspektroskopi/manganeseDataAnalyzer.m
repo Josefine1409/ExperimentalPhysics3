228aaef6bd8ac39f5483dbbbc3b9cb259f781b09
Josefine1409 <josefine_robl@hotmail.com>
2019-11-08 11:34:06 +0100

    Try at challenge task MATLAB script

--- Gammaspektroskopi/manganeseDataAnalyzer.m
+++ Gammaspektroskopi/manganeseDataAnalyzer.m
@@ -5,35 +5,44 @@ clear all; close all; format compact; clc
 time = data.data(:,1);
 channelCount = data.data(:,2);
 
-% background
-
 %% Energy calibration
-%
+E = @(channel) 0.7364*channel + 0.0625; % keV
 
 %% Histogram of data with background
 figure(1)
 hold on
     xlim([0, max(channelCount)])
     histogram(channelCount, 'BinWidth', 1)
+    peakFitter(channelCount, 1130, 1165)
 hold off
 
 %% Peak fitter
 function peakFitter(channelCount, channelToFitMin, channelToFitMax)
     % Only use data between channelToFitMin and channelToFitMax
-    channelCountNew = []
-    for j = 1:
-        if 
-            channelCountNew = [channelCountNew; ];
+    channelCountNew = [];
+    for j = 1:length(channelCount)
+        if channelCount(j) <= channelToFitMax && channelCount(j) >= channelToFitMin
+            channelCountNew = [channelCountNew; channelCount(j)];
+        end
     end
+    % Sorting the channel counts
+    channelCountNewSorted = sort(channelCountNew);
+    % Histogram to f(x)
+    channelCountNew = accumarray(channelCountNewSorted(:), 1);
     
     % Fitting function
-        % N_f = @(W) alpha * (gamma_f / ((W - M)^2 * c^4 + (gamma^2 / 4)));
-        % Background linear
-    fun = @(x) c(1) * (c(2) / ((x - c(3))^2 * c^4 + (c(4)^2 / 4));
+            % N_f = @(W) alpha * (gamma_f / ((W - M)^2 * c^4 + (gamma^2 / 4)));
+            % Background linear
+        % fun = @(k, C) k(1) * (k(2) / ((C - k(3))^2 + (k(4)^2 / 4))) + k(5)*x + k(6);
+        % Linaer + Gauss: a*x + b + a1*exp(-((x-b1)/c1)^2)
+    fun = @(k, x) k(4) .* x + k(5) + k(3) .* exp(-((x - k(1)) ./ (k(2))).^2);
 
     % Inputs to nlinfit
-    channelsToFit = channelToFitMin : channelToFitMax;
-    guess = ???;
+    channelsToFit = channelToFitMin : 1 : channelToFitMax;
+    [peakHeight, peakMid] = max(channelCountNew);
+    guess = [peakMid, 1, peakHeight, 0, 1]; % [peakMid, peakWidth, peakHeight, slope, offset];
 
-    outputFit = nlinfit(channelsToFit, channelCount, N_f, guess);
+    [values,R,J,CovB,MSE,ErrorModelInfo] = nlinfit(channelsToFit, channelCountNew, fun, guess);
+    
+    plot(channelsToFit, fun(values, channelsToFit), '-r')
 end
